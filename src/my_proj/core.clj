2cad5266f5e674576ef9444a9e4251bfcc6cd5b9
Emily Wu <embrilliant@hotmail.com>
2016-01-03 00:34:04 +0100

    first commit

---
+++ src/my_proj/core.clj
@@ -0,0 +1,14 @@
+(ns my-proj.core
+  (:gen-class)
+)
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!")
+)
+
+(defn whatIs
+	[para1]
+	(println "What is" para1 "actually?")
+)
